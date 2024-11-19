95ef600dcf6c7ae74df41f46c269a580a82f3980
hanselmeinhard17 <hanselmeinhard17@users.noreply.github.com>
2024-11-19 19:23:21 +0700

    Sorry ce saya lupa commit satu-satu, jadi ini saya langsung yang paling akhir.

--- app/src/main/java/uts/c14220233/recyclerview/wayang.kt
+++ app/src/main/java/uts/c14220233/recyclerview/wayang.kt
@@ -1,5 +1,6 @@
 package uts.c14220233.recyclerview
 
+import android.os.Parcelable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -7,10 +8,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class wayang(
     var foto : String,
     var nama : String,
     var karakter : String,
     var deskripsi : String
-)
+) : Parcelable
